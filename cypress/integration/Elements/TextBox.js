d2070cb09ce0965e7e5c911248f0bedf72678de9
Auntor Acharja <acharjeeauntor@gmail.com>
2022-01-14 21:39:10 +0600

    Added Allure Report to the project

--- cypress/integration/Elements/TextBox.js
+++ cypress/integration/Elements/TextBox.js
@@ -1,36 +1,38 @@
 /// <reference types="cypress"/>
 
 import { TextBoxPage } from "../../PageObjects/TextBoxPage";
-describe('Test Text Box Feature',()=>{
-    before(()=>{
-        cy.visit("text-box")                   
-    })
 
-
-    beforeEach(()=>{
-        cy.fixture('testData').as('data')
-    })
-
-it("Verify Placeholders are visiable or not",()=>{
-   cy.findElementByXpath("//input[@id='userName']").invoke('attr','placeholder').should('eq','Full Name')
-   cy.findElementByXpath("//input[@id='userEmail']").invoke('attr','placeholder').should('eq','name@example.com')
-   cy.get("#currentAddress").invoke('attr','placeholder').should('eq','Current Address')
-
+before(() => {
+    cy.visit("text-box")
+    cy.fixture('testData').as('data')
 })
 
-    it("Verify Text box is working properly or not using valid and invalid input",function(){
-        //cy.log(this.data.fullname)
-        const textBox = new TextBoxPage()
-        textBox.enterFullName(this.data.textBoxData.fullname)
-        textBox.enterEmail(this.data.textBoxData.email)
-        textBox.enterCurrentAddress(this.data.textBoxData.currentAddress)
-        textBox.enterPermanentAddress(this.data.textBoxData.permanentAddress)
-        textBox.clickSubmitButton()
 
-        cy.xpath("//p[@id='name']").should('contain',this.data.textBoxData.fullname)
-        cy.xpath("//p[@id='email']").should('contain',this.data.textBoxData.email)
-        cy.xpath("//p[@id='currentAddress']").should('contain',this.data.textBoxData.currentAddress)
-        cy.xpath("//p[@id='permanentAddress']").should('contain',this.data.textBoxData.permanentAddress)
+describe('Test Text Box Feature', () => {
+
+    it("Verify Placeholders are visiable or not", () => {
+        cy.findElementByXpath("//input[@id='userName']").invoke('attr', 'placeholder').should('eq', 'Full Name')
+        cy.findElementByXpath("//input[@id='userEmail']").invoke('attr', 'placeholder').should('eq', 'name@example.com')
+        cy.get("#currentAddress").invoke('attr', 'placeholder').should('eq', 'Current Address')
+
+    })
+
+    it("Verify Text box is working properly or not using valid and invalid input", function () {
+        const textBox = new TextBoxPage()
+        const textBoxDataLength = this.data.textBoxData.length
+        for (var i = 0; i < textBoxDataLength; i++) {
+            textBox.enterFullName(this.data.textBoxData[i].fullname)
+            textBox.enterEmail(this.data.textBoxData[i].email)
+            textBox.enterCurrentAddress(this.data.textBoxData[i].currentAddress)
+            textBox.enterPermanentAddress(this.data.textBoxData[i].permanentAddress)
+            textBox.clickSubmitButton()
+
+            cy.xpath("//p[@id='name']").should('contain', this.data.textBoxData[i].fullname)
+            cy.xpath("//p[@id='email']").should('contain', this.data.textBoxData[i].email)
+            cy.xpath("//p[@id='currentAddress']").should('contain', this.data.textBoxData[i].currentAddress)
+            cy.xpath("//p[@id='permanentAddress']").should('contain', this.data.textBoxData[i].permanentAddress)
+        }
+
 
 
     })
