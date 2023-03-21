a904439f937958175397eea3bd01fb19e341ab9f
Auntor Acharja <acharjeeauntor@gmail.com>
2023-03-21 22:59:52 +0600

    Update Cypres and others packages and run again

---
+++ cypress.config.js
@@ -0,0 +1,27 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  projectId: 'wbs7da',
+  video: false,
+  env: {
+    projectURL: 'https://demoqa.com/',
+    browserSupport: 'Chrome',
+    QA_Name: 'Auntor Acharja',
+  },
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    baseUrl: 'https://demoqa.com/',
+    specPattern: [
+      'cypress/e2e/Elements/*.js',
+      'cypress/e2e/Forms/*.js',
+      'cypress/e2e/AlertFrameWindows/*.js',
+      'cypress/e2e/Widgets/*js',
+      'cypress/e2e/Interactions/*.js',
+      'cypress/e2e/BookStoreApplication/*.js',
+    ],
+  },
+})
